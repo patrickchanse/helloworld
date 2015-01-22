69416416529b3b1f209d43be2e704584331274e7
Patrick Chan <patrick@Patrick-Chans-MacBook-Pro.local>
2015-01-22 11:52:14 +0100

    Fixed copy and added watch to default gulp task

--- gulp-tasks/default.js
+++ gulp-tasks/default.js
@@ -1,3 +1,3 @@
 gulp.task('default', ['clean'], function () {
-    gulp.start('templates', 'styles', 'scripts', 'images', 'copy');
+    gulp.start('templates', 'styles', 'scripts', 'images', 'copy', 'watch');
 });
