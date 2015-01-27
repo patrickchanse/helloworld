0a355a5ced5bcd52b77338d2ce0e5cd30cc3ce2f
Patrick Chan <patrick@Patrick-Chans-MacBook-Pro.local>
2015-01-27 16:29:55 +0100

    update

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -1,12 +1,17 @@
 // Load plugins
 var gulpif = require('gulp-if'),
 	stylus = require('gulp-stylus'),
+	nib = require('nib'),
+	rupture = require('rupture'),
 	jeet = require('jeet');
 
 gulp.task('styles', function() {
     return gulp.src([path.src + '/assets/styles/app.styl'])
         .pipe(stylus({
-            use: [jeet()]
+            use: [nib(), rupture(), jeet()],
+            'include css': true,
+            linenos: true,
+            compress: true
         }))
         .on('error', onError)
         .pipe(gulp.dest(path.dest + '/assets/css'));
