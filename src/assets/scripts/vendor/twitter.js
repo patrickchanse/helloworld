8cd076be39ca588f836eab13095433d9a1f45063
Patrick Chan <patrick@Patrick-Chans-MacBook-Pro.local>
2015-01-28 18:32:00 +0100

    fixed soaces to tabs and fonts

--- src/assets/scripts/vendor/twitter.js
+++ src/assets/scripts/vendor/twitter.js
@@ -1,7 +1,7 @@
 (function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//platform.twitter.com/widgets.js";
-   fjs.parentNode.insertBefore(js, fjs);
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement(s); js.id = id;
+	js.src = "//platform.twitter.com/widgets.js";
+	fjs.parentNode.insertBefore(js, fjs);
  }(document, 'script', 'twitter-widgets'));
